b1e017ac743240592b618d0df9fcd85eb49daf07
Inz_All_Gone <husnutdinovdanis40@gmail.com>
2023-09-07 16:23:52 +0300

    sm2

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+
+// запрос инфы у поьльзователя
+
+let city_name = prompt ('Название города');
+let city_year = prompt ('Год образования города');
+let city_people = prompt ('Какое население города');
+let city_age = 2023;
+let x = city_year;
+let city_count = (city_age - x);
+
+alert ( 'Город:' + city_name + ',Возраст:' +  city_count + 'лет городу с момента появления,'
+ + 'Население:' + city_people + 'человек' );
+
+
